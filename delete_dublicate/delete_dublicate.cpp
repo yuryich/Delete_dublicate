f230a4f7bf518734f0f70db7b93b2735fd7276f1
Neto test <yury120405@gmail.com>
2024-10-05 21:22:27 +0400

    Initial commit

---
+++ delete_dublicate/delete_dublicate.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <set>
+#include <vector>
+#include <algorithm>
+
+int main() {
+	int n;
+
+	std::cin >> n;
+
+	std::set<int> unique_numbers;
+	for (int i = 0; i < n; ++i) {
+		int x;
+		std::cin >> x;
+		unique_numbers.insert(x);
+	}
+
+	std::vector<int> sorted_numbers(unique_numbers.begin(), unique_numbers.end());
+	std::sort(sorted_numbers.rbegin(), sorted_numbers.rend());
+
+	for (int num : sorted_numbers) {
+		std::cout << num << std::endl;
+	}
+
+}
